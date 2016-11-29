5b3566a8f703da93c5a625a2fbcfdd4f97b99f93
bcronce <bcronce@gmail.com>
2016-11-28 22:08:22 -0600

    Place-holder for IDisposible

--- BoringFlatFileParser/Parser.cs
+++ BoringFlatFileParser/Parser.cs
@@ -7,7 +7,7 @@ using System.Threading;
 
 namespace BFFP
 {
-    public abstract class Parser
+    public abstract class Parser : IDisposable
     {
         public Task<bool> Read(DataRow reuse, CancellationToken cancellationToken)
         {
@@ -24,7 +24,51 @@ namespace BFFP
 
         public DataRow GetRecord()
         {
-            throw new NotImplementedException();
+            var tmpRecord = this.Record;
+
+            //Throw a tantrum if the record is not set
+            if (tmpRecord.Fields == null)
+                throw new InvalidOperationException($"Cannot call {nameof(GetRecord)} without {nameof(Read)} returning true");
+
+            //Reset once consumed
+            this.Record = new DataRow(null, null);
+
+            return tmpRecord;
         }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~Parser() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
