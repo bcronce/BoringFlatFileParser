7d67772a12245f42ac9548d642b30f957d4c22ae
bcronce <bcronce@gmail.com>
2016-11-28 18:24:17 -0600

    New struct to hold the offset and length of a field

---
+++ BoringFlatFileParser/Field.cs
@@ -0,0 +1,20 @@
+﻿namespace BFFP
+{
+    public struct Field
+    {
+        public int Offset;
+        public int Length;
+
+        public Field(int offset, int length)
+        {
+            this.Offset = offset;
+            this.Length = length;
+        }
+
+        public Field(Field field)
+        {
+            this.Offset = field.Offset;
+            this.Length = field.Length;
+        }
+    }
+}
