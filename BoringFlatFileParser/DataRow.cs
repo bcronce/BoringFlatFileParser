2a43818df1b628dc2bf5b146376be8a86568eeb5
bcronce <bcronce@gmail.com>
2016-11-28 18:26:18 -0600

    New struct to represent a data record

---
+++ BoringFlatFileParser/DataRow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace BFFP
+{
+    public struct DataRow
+    {
+        public List<Field> Fields;
+        public char[] Buffer;
+
+        public DataRow(List<Field> fields, char[] buffer)
+        {
+            this.Fields = fields;
+            this.Buffer = buffer;
+        }
+
+        public DataRow(DataRow dataRow)
+        {
+            this.Fields = dataRow.Fields;
+            this.Buffer = dataRow.Buffer;
+        }
+    }
+}
