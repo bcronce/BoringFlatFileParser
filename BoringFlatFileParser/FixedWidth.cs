d0cf9f37889429b2791e4359c1fd82d6800602e2
bcronce <bcronce@gmail.com>
2016-12-04 13:42:04 -0600

    Starting work on Fixed Width parser

---
+++ BoringFlatFileParser/FixedWidth.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BFFP
+{
+    public class FixedWidth : Parser
+    {
+        public struct Field
+        {
+            public int Length;
+
+            public Field(int length)
+            {
+                this.Length = length;
+            }
+        }
+
+        protected Field[] Fields;
+
+        protected override Task<bool> InternalRead(DataRow reuse, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FixedWidth(StreamReader input, List<Field> fields)
+            : base(input, true)
+        {
+            this.Fields = fields.ToArray();
+        }
+    }
+}
